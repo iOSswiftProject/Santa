b43b20eec787f3888af7448e3fd8a92969aead32
lee <dog10041@naver.com>
2021-05-27 09:32:45 +0900

    Initial Commit

---
+++ santaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  santaProject
+//
+//  Created by 이병훈 on 2021/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
