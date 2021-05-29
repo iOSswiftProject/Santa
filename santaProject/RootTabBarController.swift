a376e09ff30c5a624bc9a8678ff501a349a16066
journey <wndnjs1769@naver.com>
2021-05-29 20:55:23 +0900

    Create RootTabBarController

---
+++ santaProject/RootTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  RootTabBarController.swift
+//  santaProject
+//
+//  Created by Journey on 2021/05/29.
+//
+
+import UIKit
+
+class RootTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .brown
+    }
+}
