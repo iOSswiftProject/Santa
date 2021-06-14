ba3d8f00a7a61db73baaf8adc708f9672b32cbcb
journey <wndnjs1769@naver.com>
2021-06-14 20:12:20 +0900

    Create MyListCollectionViewCell

---
+++ santaProject/MyList/MyListCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MyListCollectionViewCell.swift
+//  santaProject
+//
+//  Created by Juwon Kim on 2021/06/13.
+//
+
+import UIKit
+
+class MyListCollectionViewCell: UICollectionViewCell {
+    static let identifier = "MyListCollectionViewCell"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
