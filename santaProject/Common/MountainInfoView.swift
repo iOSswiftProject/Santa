3957227cddb5273c21ce491dccc77e7dbc8adbe1
journey <wndnjs1769@naver.com>
2021-06-22 00:25:06 +0900

    Create common MountainInfoView

---
+++ santaProject/Common/MountainInfoView.swift
@@ -0,0 +1,19 @@
+//
+//  MountainInfoView.swift
+//  santaProject
+//
+//  Created by Juwon Kim on 2021/06/22.
+//
+
+import UIKit
+
+class MountainInfoView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
