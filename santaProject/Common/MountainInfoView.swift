0f9bb08fc6a1354e27d17b8cef1fbfc198eeaa99
journey <wndnjs1769@naver.com>
2021-06-26 12:54:42 +0900

    Add mountainNameLabel and separatorView

--- santaProject/Common/MountainInfoView.swift
+++ santaProject/Common/MountainInfoView.swift
@@ -19,11 +19,67 @@ struct MockDataModel {
 
 class MountainInfoView: UIView {
 
+    let mountainNameLabel = UILabel()
+    let separatorView = UIView()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupMountainNameLabel()
+        setupSeparatorView()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupMountainNameLabel() {
+        addSubview(mountainNameLabel)
+        mountainNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        mountainNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Layout.MountainNameLabel.sideMargin).isActive = true
+        mountainNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: Layout.MountainNameLabel.topMargin).isActive = true
+        updateMountainNameLabelAttributedText(mountainName: "관악산", peakName: "땡땡봉")
+        mountainNameLabel.sizeToFit()
+    }
+
+    private func setupSeparatorView() {
+        addSubview(separatorView)
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+        separatorView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Layout.Separator.sideMargin).isActive = true
+        separatorView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Layout.Separator.sideMargin).isActive = true
+        separatorView.topAnchor.constraint(equalTo: topAnchor, constant: Layout.Separator.topMargin).isActive = true
+        separatorView.heightAnchor.constraint(equalToConstant: Layout.Separator.height).isActive = true
+        separatorView.backgroundColor = UIColor(hex: "EEEEEE")
+    }
+}
+
+extension MountainInfoView {
+    private func updateMountainNameLabelAttributedText(mountainName: String, peakName: String?) {
+        let text = mountainName + " " + (peakName ?? "")
+        let textAsNSString = text as NSString
+        let attributedString = NSMutableAttributedString(string: text)
+        attributedString.addAttribute(.font,
+                                      value: Layout.MountainNameLabel.mountainFont,
+                                      range: textAsNSString.range(of: mountainName))
+        attributedString.addAttribute(.font,
+                                      value: Layout.MountainNameLabel.peakFont,
+                                      range: textAsNSString.range(of: peakName ?? ""))
+        mountainNameLabel.attributedText = attributedString
+    }
+}
+
+extension MountainInfoView {
+    private enum Layout {
+        enum MountainNameLabel {
+            static let sideMargin: CGFloat = 24
+            static let topMargin: CGFloat = 17
+            static let fontSize: CGFloat = 20
+            static let mountainFont = UIFont.systemFont(ofSize: fontSize, weight: UIFont.Weight(0.4))
+            static let peakFont = UIFont.systemFont(ofSize: fontSize, weight: UIFont.Weight(0.2))
+        }
+        enum Separator {
+            static let sideMargin: CGFloat = 20
+            static let topMargin: CGFloat = 54
+            static let height: CGFloat = 1
+        }
     }
 }
