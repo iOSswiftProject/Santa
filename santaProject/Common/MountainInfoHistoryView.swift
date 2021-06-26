886e13c85a2ff17be3e2b5f519df9a36f5694589
journey <wndnjs1769@naver.com>
2021-06-26 16:53:42 +0900

    Create MountainInfoHistoryView

---
+++ santaProject/Common/MountainInfoHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  MountainInfoHistoryView.swift
+//  santaProject
+//
+//  Created by Juwon Kim on 2021/06/26.
+//
+
+import UIKit
+
+class MountainInfoHistoryView: MountainInfoBaseView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
