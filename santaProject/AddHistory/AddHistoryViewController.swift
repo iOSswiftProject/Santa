877b0667e7bb5bbebef7b9c7b7aaa612bbe36fd4
journey <wndnjs1769@naver.com>
2021-07-10 14:21:01 +0900

    Apply view for AddHistoryViewController

--- santaProject/AddHistory/AddHistoryViewController.swift
+++ santaProject/AddHistory/AddHistoryViewController.swift
@@ -8,5 +8,34 @@
 import UIKit
 
 class AddHistoryViewController: UIViewController {
-    
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        let view = AddHistoryView()
+        view.delegate = self
+        self.view = view
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension AddHistoryViewController: AddHistoryViewDelegate {
+    func didTapDoneButton() {
+        print("didTapDoneButton()")
+        dismiss(animated: true, completion: nil)
+    }
+
+    func didTapCancelButton() {
+        print("didTapCancelButton()")
+        dismiss(animated: true, completion: nil)
+    }
 }
