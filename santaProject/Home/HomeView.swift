77e7e160188fadf11c75294e39ecc556873a3653
journey <wndnjs1769@naver.com>
2021-07-26 16:31:15 +0900

    Create home view group

---
+++ santaProject/Home/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  santaProject
+//
+//  Created by Juwon Kim on 2021/07/26.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
