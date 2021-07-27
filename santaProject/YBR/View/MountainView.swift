62379662a2a5eeb4ba061430b2f9a1f80807cc6c
Ro <yb359kr@gmail.com>
2021-07-28 00:07:54 +0900

    modify callout UI

--- santaProject/YBR/View/MountainView.swift
+++ santaProject/YBR/View/MountainView.swift
@@ -24,6 +24,7 @@ class MountainView: MKAnnotationView {
             if mountain.isFavorite ?? false {
                 addFavoriteButton()
             }
+            setupCalloutView(mountain)
 
         }
     }
@@ -58,4 +59,81 @@ class MountainView: MKAnnotationView {
         image = UIImage(named: imgName)
 
     }
+    
+    func setupCalloutView(_ annotation: MKAnnotation) {
+        
+        self.canShowCallout = true
+        
+        let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        btn.setBackgroundImage(UIImage(named: "santaIconArrow"), for: .normal)
+        
+        btn.addTarget(self, action: #selector(touchRightButton), for: .touchUpInside)
+        self.rightCalloutAccessoryView = btn
+
+    }
+    
+    @objc
+    func touchRightButton(sender: UIButton) {
+        print("callout Button touch!")
+    }
+    
+    
+    //FIXME: 임시코드. 개선할 것
+    override func layoutSubviews() {
+        if !self.isSelected {
+            return
+        }
+        
+        for v in self.subviews {
+            setLabelOfCallout(view: v)
+        }
+//        if self.subviews.count > 0 {
+//            let calloutView = self.subviews[0]
+//            setLabelOfCallout(view: calloutView)
+//            if calloutView.subviews.count > 0 {
+//                let bubbleView = calloutView.subviews[0]
+//
+//                setLabelOfCallout(view: bubbleView)
+//            }
+//        }
+
+    }
+
+    func setLabelOfCallout(view: UIView) {
+        print("loop")
+        print(view)
+        
+        //UILabel만 찾아 superView background color 바꾸기
+        for v in view.subviews {
+            
+            if v.isKind(of: UILabel.self) {
+                view.backgroundColor = .green
+                
+                guard let label = v as? UILabel, let text = label.text else { return }
+
+                label.attributedText = setToAttributedString(str: text)
+                
+                
+                return
+            }
+            
+            setLabelOfCallout(view: v)
+        }
+    }
+    
+    
+    
+    func setToAttributedString(str: String) -> NSMutableAttributedString {
+        let attributedString = NSMutableAttributedString(string: (str), attributes: [.font: UIFont.systemFont(ofSize: 14, weight: .medium), .foregroundColor: UIColor.black, .kern: 1.0])
+        
+        if let idx = str.firstIndex(of: " ") {
+            
+            let pos = str.distance(from: str.startIndex, to: idx)
+            attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSMakeRange(pos, str.count-pos))
+            
+        }
+
+        return attributedString
+    }
+    
 }
