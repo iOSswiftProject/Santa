36da3e731565a3e34a39910038b70a28d0cf38df
journey <wndnjs1769@naver.com>
2021-08-16 17:57:35 +0900

    Apply navigationController for List view

--- santaProject/Detail/DetailViewController.swift
+++ santaProject/Detail/DetailViewController.swift
@@ -9,13 +9,28 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    private var mountain: Mountain
+
+    init(with mountain: Mountain) {
+        self.mountain = mountain
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        view.backgroundColor = .white
 
         let label = UILabel()
-        label.text = "상세보기"
+        label.text = mountain.name
         view.addSubview(label)
         label.frame = CGRect(x: 100, y: 400, width: 200, height: 200)
     }
