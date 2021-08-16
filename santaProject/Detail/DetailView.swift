31c53762aa04cea14082f85b3bdf5757fde43b68
journey <wndnjs1769@naver.com>
2021-08-16 18:09:40 +0900

    Add DetailView and DetailViewModel files

---
+++ santaProject/Detail/DetailView.swift
@@ -0,0 +1,19 @@
+//
+//  DetailView.swift
+//  santaProject
+//
+//  Created by Juwon Kim on 2021/08/16.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ santaProject/Detail/DetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewModel.swift
+//  santaProject
+//
+//  Created by Juwon Kim on 2021/08/16.
+//
+
+import Foundation
+
+class DetailViewModel {
+    
+}
