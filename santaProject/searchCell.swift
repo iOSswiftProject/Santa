e74d8bd68dfb6682b5d1a74c948e334a18cb5d6e
Ro <yb359kr@gmail.com>
2021-09-13 22:19:05 +0900

    검색화면에 현재 동네의 산위치 찍어주기

--- santaProject/searchCell.swift
+++ santaProject/searchCell.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
+protocol SearchCellDelegate {
+    func deleteButtonDidTouched(cell: searchCell)
+}
+
 class searchCell: UITableViewCell {
     var mountainLabel: UILabel!
     var accessoryImage: UIImageView!
     //2번쨰 방법
     var deleteButton: UIButton!
     let margin: CGFloat = 20
-    
+    var delegate: SearchCellDelegate?
+    var idx: Int?
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -59,7 +64,93 @@ class searchCell: UITableViewCell {
         deleteButton.translatesAutoresizingMaskIntoConstraints = false
         deleteButton.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -margin).isActive = true
         deleteButton.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: margin).isActive = true
+        
+        deleteButton.addTarget(self, action: #selector(touchDeleteButton), for: .touchUpInside)
+    }
+    
+    @objc
+    func touchDeleteButton() {
+        delegate?.deleteButtonDidTouched(cell: self)
     }
 
     
 }
+
+class SearchResultCell: UITableViewCell {
+
+    var mountain: Mountain? {
+        didSet {
+            update()
+        }
+    }
+    
+    var mountainImage = UIImageView()
+    var visitImage = UIImageView()
+    var mountainLabel = UILabel()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        makeMountainBadge()
+        makeVisitBadge()
+        makeMountainLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func makeMountainBadge() {
+//        mountainImage = UIImageView.init()
+        
+        mountainImage.image = UIImage(named: "santaTabImageListInactive")
+        self.contentView.addSubview(mountainImage)
+        mountainImage.translatesAutoresizingMaskIntoConstraints = false
+        mountainImage.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
+        mountainImage.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
+        mountainImage.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
+        mountainImage.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        mountainImage.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        mountainImage.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
+    }
+    
+    func makeVisitBadge() {
+//        visitImage = UIImageView.init()
+        // 이미지 설정
+        visitImage.image = UIImage(named: "santaFlagInactive")
+        self.contentView.addSubview(visitImage)
+        visitImage.translatesAutoresizingMaskIntoConstraints = false
+//        visitImage.leadingAnchor.constraint(equalTo: ).isActive = true
+        visitImage.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 32).isActive = true
+        visitImage.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 20).isActive = true
+        visitImage.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -20).isActive = true
+        visitImage.widthAnchor.constraint(equalToConstant: 18).isActive = true
+//        visitImage.heightAnchor.constraint(equalToConstant: 18).isActive = true
+        visitImage.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
+    }
+    
+    func makeMountainLabel() {
+        //register도 무조건 설정
+        self.contentView.addSubview(mountainLabel)
+        mountainLabel.font = UIFont.systemFont(ofSize: 13)
+
+        mountainLabel.translatesAutoresizingMaskIntoConstraints = false
+        mountainLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 20).isActive = true
+        mountainLabel.leadingAnchor.constraint(equalTo: visitImage.trailingAnchor, constant: 13).isActive = true
+    }
+    
+    func update() {
+        var favoriteImageName = "santaTabImageListInactive"
+        if let isFavorite = mountain?.isFavorite, isFavorite {
+            favoriteImageName = "santaTabImageListActive"
+        }
+        
+        var visitImageName = "santaTabImageListInactive"
+        if let isVisit = mountain?.isVisit, isVisit {
+            visitImageName = "santaFlagActive"
+        }
+        visitImage.image = UIImage(named: visitImageName)
+        mountainImage.image = UIImage(named: favoriteImageName)
+    }
+    
+}
+
