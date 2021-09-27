f83e320fe9287e30812f6087d5ddc071d061f419
Ro <yb359kr@gmail.com>
2021-09-27 20:39:26 +0900

    최근검색어 용량제한, 중복처리 추가

--- santaProject/searchBarData.swift
+++ santaProject/searchBarData.swift
@@ -8,8 +8,32 @@
 import UIKit
 
 class searchBarData {
-    var searchItems: [String] = []
+//    var searchItems: [String] = []
 //    let localItems: [String] = ["설악산","지리산","무등산","아무산","그냥산","앞산","뒷산","그산","오른쪽산"]
+
+    var searchItems: [String] = []{
+        
+        didSet {
+            if searchItems.count == 0 {
+                return
+            }
+            //1. 중복체크. 기존에 존재 시 제거.
+            let latesName = searchItems.last
+            for i in 0..<searchItems.count-1 {
+                if latesName == searchItems[i] {
+                    searchItems.remove(at: i)
+                }
+            }
+            
+            // 10개로 용량제한
+            if searchItems.count > 10 {
+                searchItems.removeFirst()
+            }
+            
+        }
+        
+    }
+    
     let localItems: [String] = {
         var mountainArr = [String]()
         let mountains = DBInterface.shared.selectMountain()
