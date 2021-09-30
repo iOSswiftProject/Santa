32dfd560c8f7fff72e4e37ee9cce1d6e44969f04
journey <wndnjs1769@naver.com>
2021-09-30 22:33:59 +0900

    Make navigation bar to be transparent

--- santaProject/Detail/DetailViewController.swift
+++ santaProject/Detail/DetailViewController.swift
@@ -13,8 +13,12 @@ class DetailViewController: UIViewController {
     private let detailViewModel: DetailViewModel
     private let detailView = DetailView()
 
-    private var navigationBarHidden: Bool?
-    private var tabbarHidden: Bool?
+    private var navigationBarHidden = true
+    private var tabbarHidden = true
+    private var naviBackButtonTitle: String?
+    private var naviTintColor: UIColor?
+    private var naviBackIndicatorImage: UIImage?
+    private var naviBackIndicatorTransitionMaskImage: UIImage?
 
     init(with mountain: Mountain) {
         self.mountain = mountain
@@ -28,21 +32,58 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        navigationBarHidden = navigationController?.isNavigationBarHidden
-        tabbarHidden = tabBarController?.tabBar.isHidden
-        navigationController?.setNavigationBarHidden(true, animated: false)
-        tabBarController?.tabBar.isHidden = true
-
         setupDetailView()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        navigationBarHidden = navigationController?.isNavigationBarHidden ?? true
+        tabbarHidden = tabBarController?.tabBar.isHidden ?? true
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        tabBarController?.tabBar.isHidden = true
+
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationBar.shadowImage = UIImage()
+        navigationBar.backgroundColor = .clear
+
+        naviBackButtonTitle = navigationBar.backItem?.backButtonTitle
+        navigationBar.backItem?.backButtonTitle = ""
+
+        naviTintColor = navigationBar.tintColor
+        navigationBar.tintColor = .stCoolGray70
+
+        naviBackIndicatorImage = navigationBar.backIndicatorImage
+        naviBackIndicatorTransitionMaskImage = navigationBar.backIndicatorTransitionMaskImage
+        navigationBar.backIndicatorImage = UIImage(named: "santaBackButton")
+        navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "santaBackButton")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        navigationController?.setNavigationBarHidden(navigationBarHidden, animated: true)
+        tabBarController?.tabBar.isHidden = tabbarHidden
+
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        navigationBar.setBackgroundImage(nil, for: .default)
+        navigationBar.shadowImage = nil
+        navigationBar.backgroundColor = nil
+        navigationBar.backItem?.backButtonTitle = naviBackButtonTitle
+        navigationBar.tintColor = naviTintColor
+
+        navigationBar.backIndicatorImage = naviBackIndicatorImage
+        navigationBar.backIndicatorTransitionMaskImage = naviBackIndicatorTransitionMaskImage
+    }
+
     private func setupDetailView() {
+        let naviBarHeight = navigationController?.navigationBar.frame.height ?? 0
         view.addSubview(detailView)
         detailView.translatesAutoresizingMaskIntoConstraints = false
         detailView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         detailView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -naviBarHeight).isActive = true
         detailView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         detailView.delegate = self
         detailViewModel.congifure(detailView)
